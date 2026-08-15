c7ed9dcbafb55a394575b2ca662507027c805ee8
RodrigoFaria84 <rodrigofsilva84@outlook.com>
2026-08-14 21:11:49 -0300

    adiciona pipeline para aws

---
+++ src/main/java/br/com/cotiinformatica/api_solicitacoes/infrastructure/components/MessageProducerComponent.java
@@ -0,0 +1,23 @@
+package br.com.cotiinformatica.api_solicitacoes.infrastructure.components;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MessageProducerComponent {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final Queue queue;
+
+    public void sendMessage(String message) throws Exception {
+        try {
+            rabbitTemplate.convertAndSend(queue.getName(), message);
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
